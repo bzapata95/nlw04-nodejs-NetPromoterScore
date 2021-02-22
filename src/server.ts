729dfa74dd91a68a714b6dd2de11aa54578ef530
Bryan Zapata <bmzc95@gmail.com>
2021-02-22 10:59:49 -0500

    first commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/users", (request, response) => {});
+
+app.listen(3333, () => {
+  console.log("⚡️ Server run on port 3333");
+});
